a1c9c94d92f13496bb5dc3c3a5ff22caae18ee2f
wmufunde <wmmufunde@yahoocom>
2017-02-19 11:57:24 +0000

    settings

--- comicsList/settings.py
+++ comicsList/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
