ce404ece6aee56b42af66f2d945769fedb5ea69e
wmufunde <wmmufunde@yahoocom>
2017-02-20 23:26:44 +0000

    settings

--- comicsList/settings.py
+++ comicsList/settings.py
@@ -126,9 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
