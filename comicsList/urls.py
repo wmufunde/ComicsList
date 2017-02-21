722cb9c5c7d6995d2b9a82dc9903a929d90c04dd
wmufunde <wmmufunde@yahoocom>
2017-02-21 00:10:05 +0000

    update.urls.py

--- comicsList/urls.py
+++ comicsList/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 #from listOfComics import views as listOfComics_views
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('django.contrib.auth.urls')),
     url(r'^listOfComics/', include('listOfComics.urls', namespace='listOfComics'))
